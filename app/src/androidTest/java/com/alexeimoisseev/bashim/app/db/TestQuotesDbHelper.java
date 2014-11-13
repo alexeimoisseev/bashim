40215a35393c0b2d9a1db3c866844408ecbc1d1d
Alexei Moisseev <amois@yandex-team.ru>
2014-11-13 19:17:11 +0300

    added TestQuotesDbHelper

---
+++ app/src/androidTest/java/com/alexeimoisseev/bashim/app/db/TestQuotesDbHelper.java
@@ -0,0 +1,56 @@
+package com.alexeimoisseev.bashim.app.db;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import com.alexeimoisseev.bashim.app.activities.MainActivity;
+import com.alexeimoisseev.bashim.app.beans.QuoteBean;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by amois on 10.11.14.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+public class TestQuotesDbHelper {
+
+    QuotesDbHelper helper = new QuotesDbHelper(new MainActivity());
+
+
+    @Test
+    public void testGetReadableDatabase() {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Assert.assertTrue(db.isOpen());
+    }
+
+    @Test
+    public void testGetSavedQuotes() {
+        List<QuoteBean> quotes = helper.getSavedQuotes(0);
+        Assert.assertNotNull(quotes);
+    }
+
+    @Test
+    public void testClearQuotesTable() {
+        helper.clearQuotesTable();
+        Assert.assertEquals(0, helper.getSavedQuotes(0).size());
+
+    }
+
+    @Test
+    public void testSaveQuotes() {
+        helper.clearQuotesTable();
+        List<QuoteBean> quotes = new ArrayList<QuoteBean>();
+        QuoteBean quote = new QuoteBean();
+        quote.setId(1l);
+        quotes.add(quote);
+        helper.saveQuotes(quotes);
+        Assert.assertEquals(1, helper.getSavedQuotes(0).size());
+    }
+}
